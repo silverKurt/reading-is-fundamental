05f919ada184002975547868d09e4c380eb68776
silverKurt <paulo1196@gmail.com>
2018-08-06 17:45:46 -0300

    FiltroPronto

--- ReadingIsFundamental3.0/src/servlet/AcaoAlunos.java
+++ ReadingIsFundamental3.0/src/servlet/AcaoAlunos.java
@@ -6,12 +6,10 @@ import entidades.Aluno;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@WebServlet(urlPatterns = "/cadastro_alunos")
 public class AcaoAlunos extends HttpServlet {
 
     @Override
---
+++ ReadingIsFundamental3.0/src/servlet/Filtro.java
@@ -0,0 +1,86 @@
+package servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebFilter("/*")
+public class Filtro extends HttpServlet implements Filter {
+    List<String> urls = new ArrayList<>();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet filtro</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet filtro at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        urls.add("/ReadingIsFundamentalWeb/");
+        urls.add("/ReadingIsFundamentalWeb/acao");
+        urls.add("/ReadingIsFundamentalWeb/index.jsp");
+        urls.add("/ReadingIsFundamentalWeb/login.jsp");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        // verifica se URL requisitada consta na lista de URLs permitidas
+        // caso afirmativo, envia para página de Login
+        if (urls.contains(req.getRequestURI())) {
+            request.setAttribute("parametro", "login");
+            chain.doFilter(request, response);
+        } else {
+            HttpSession sessao = ((HttpServletRequest) request).getSession();
+
+            // caso não pertença a lista, verifica se há usuário na sessão
+            // se não houver, encaminha para o Login
+            if (sessao.getAttribute("usuarioLogado") == null) {
+                ((HttpServletResponse) response).sendRedirect("login.jsp");
+            } else {
+                // se usuário estiver logado, apenas abra a página solicitada
+                System.out.println("Destino: " + req.getRequestURI());
+                chain.doFilter(request, response);
+            }
+        }
+    }
+}
